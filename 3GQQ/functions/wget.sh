0b2df5d24a1323c3848c9c64de3158aa443746e8
zhiyb <yz39g13@ecs.soton.ac.uk>
2014-12-09 09:18:49 +0000

    3GQQ wget parameters modified for better response

--- 3GQQ/functions/wget.sh
+++ 3GQQ/functions/wget.sh
@@ -2,7 +2,7 @@
 function getData
 {
 	#	-t 5 -T 5
-	wget --dns-timeout=1 --connect-timeout=6 --read-timeout=15 --wait=0 --tries=3 --no-cache -qO - --post-data="$3" "$1" #--keep-session-cookies --load-cookies="$2" --save-cookies="$2"
+	wget --dns-timeout=1 --connect-timeout=6 --read-timeout=15 --wait=0 --tries=5 --no-cache -qO - --post-data="$3" "$1" #--keep-session-cookies --load-cookies="$2" --save-cookies="$2"
 }
 
 # Args: URL Cookies Post
