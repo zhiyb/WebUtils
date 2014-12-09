e2ef9a851b7261f567561d3ab7b23935d8cf414f
zhiyb <yz39g13@ecs.soton.ac.uk>
2014-12-09 00:26:44 +0000

    3GQQ Remove wget -T 5 giveup

--- 3GQQ/functions/wget.sh
+++ 3GQQ/functions/wget.sh
@@ -2,12 +2,12 @@
 function getData
 {
 	#	-t 5 -T 5
-	wget -t 0 -T 5 -qO - --post-data="$3" "$1" #--keep-session-cookies --load-cookies="$2" --save-cookies="$2"
+	wget -qO - --post-data="$3" "$1" #--keep-session-cookies --load-cookies="$2" --save-cookies="$2"
 }
 
 # Args: URL Cookies Post
 function getDataNoSave
 {
 	#	-t 5 -T 5
-	wget -t 0 -T 5 -qO - --post-data="$3" "$1" #--load-cookies="$2"
+	wget -qO - --post-data="$3" "$1" #--load-cookies="$2"
 }
